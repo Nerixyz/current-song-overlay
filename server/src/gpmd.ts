ec36d2ceb00ba06893ba6fa05d9ecc78e7a82488
Nerixyz <nero.9@hotmail.de>
2020-07-21 23:16:07 +0200

    fix: pepege

--- server/src/gpmd.ts
+++
@@ -1,5 +0,0 @@
-import {connectWebSocket }from 'https://deno.land/std/ws/mod.ts';
-
-for await (const msg of await connectWebSocket('ws://localhost:5672')) {
-    if(typeof msg === 'string') console.log(msg);
-}
