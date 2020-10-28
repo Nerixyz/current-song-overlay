7cf2f49d516606a510e706e7455810ad2a615665
Nerixyz <nero.9@hotmail.de>
2020-10-28 20:20:33 +0100

    feat: use web animations api

--- client/src/SmolMarquee.ts
+++ client/src/SmolMarquee.ts
@@ -1,24 +1,50 @@
 import { createElement } from './utilities';
 
 export function useMarquee(el: HTMLElement, maxWidth: number, speed: number) {
-  const invisibleWrapper = createElement('div', {classes: ['invis']});
+  const invisibleWrapper = createElement('div', { classes: ['invis'] });
+  invisibleWrapper.style.transform = 'scale(0.2)';
   document.documentElement.append(invisibleWrapper);
   const observer = new MutationObserver(() => {
     const clone = el.cloneNode(true);
     invisibleWrapper.append(clone);
+    cancelAllAnimations(el);
     requestAnimationFrame(() => {
-      const {width} = invisibleWrapper.getBoundingClientRect();
+      let { width } = invisibleWrapper.getBoundingClientRect();
+      width *= 5;
 
-      el.style.setProperty('--animation-width', `${maxWidth - width}px`);
-      el.style.setProperty('animation-duration', `${width / speed}s`);
-
-      if(width > maxWidth) {
-        el.classList.add('mq-animating');
-      } else {
-        el.classList.remove('mq-animating');
+      if (width > maxWidth) {
+        const animationWidth = maxWidth - width;
+        el.animate([{
+          transform: 'translateX(0)',
+          opacity: '1',
+          easing: 'cubic-bezier(0.3, 0, 0.7, 1)'
+        }, {
+          transform: `translateX(${animationWidth}px)`,
+          opacity: '1',
+          offset: 0.4,
+        }, {
+          transform: `translateX(${animationWidth}px)`,
+          opacity: '0',
+          offset: 0.45,
+        }, {
+          transform: 'translateX(0)',
+          opacity: '0',
+          offset: 0.455,
+        }, {
+          opacity: '1',
+          offset: 0.5
+        }], {
+          iterations: Infinity,
+          duration: (width / speed) * 1000
+        });
       }
+
       invisibleWrapper.removeChild(clone);
     });
   });
-  observer.observe(el, {subtree: true, childList: true});
+  observer.observe(el, { subtree: true, childList: true });
+}
+
+function cancelAllAnimations(el: HTMLElement) {
+  el.getAnimations().forEach(a => a.cancel());
 }
