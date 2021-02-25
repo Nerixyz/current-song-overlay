e5915b4937e759ead7c1c32330555709d3cc80a9
Nerixyz <nero.9@hotmail.de>
2021-02-25 09:29:36 +0100

    chore: remove scripts.ts

--- scripts.ts
+++
@@ -1,7 +0,0 @@
-import { ScriptsConfiguration } from 'https://deno.land/x/velociraptor@v1.0.0-beta.13/mod.ts';
-
-export default<ScriptsConfiguration> {
-    scripts: {
-        bundle: 'powershell scripts/bundle.ps1'
-    }
-}
