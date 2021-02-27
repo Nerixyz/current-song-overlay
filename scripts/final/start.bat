510a163b0b3e493a10c9fd8e638957bbc8119dff
Nerixyz <nero.9@hotmail.de>
2021-02-28 00:31:22 +0100

    fix: add --unstable (deno-namespace is unstable)

--- scripts/final/start.bat
+++ scripts/final/start.bat
@@ -1,1 +1,1 @@
-"%~dp0deno.exe" run --allow-read --allow-net --allow-env --allow-write server/index.ts
+"%~dp0deno.exe" run --allow-read --allow-net --allow-env --allow-write --unstable server/index.ts
