514a9771cbfc53eb8e3e7bc664be7b13c2d26184
Nerixyz <nero.9@hotmail.de>
2021-04-04 18:05:45 +0200

    fix: remove script after load

--- extension/src/content-scripts/mediaSessionProxy.ts
+++ extension/src/content-scripts/mediaSessionProxy.ts
@@ -5,6 +5,7 @@ const el = document.createElement('script');
 el.src = getFileUrl('content-scripts/mediaSessionProxy.inject.js');
 el.id = 'current-song-media-session-proxy';
 document.documentElement.prepend(el);
+el.addEventListener('load', () => el.remove());
 
 // pass through
 new ContentEventHandler();
